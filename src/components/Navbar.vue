7718952a13374e6c27986d35456996fe5b227047
Lord Atakora <inquire@obsidianrune.com>
2020-06-09 12:04:39 -0400

    This is the first commit, starting the sample page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="nav">
+      <nav class="black" role="navigation">
+              <div class="nav-wrapper">
+                <a href="#" class="right brand-logo">TheOtherSide</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Home</a></li>
+                    <li><a href="badges.html">Shop</a></li>
+                    <li><a href="collapsible.html">Social</a></li>
+                </ul>
+                </div>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+    nav{
+        position: fixed;
+        top:0%;
+        width: 100%;
+        z-index: 5;
+    }
+</style>
