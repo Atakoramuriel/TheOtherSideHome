25e343f8b8dd0f65e500b8d1b9caec76c1811fb4
Lord Atakora <inquire@obsidianrune.com>
2020-06-12 02:27:59 -0400

    Finished styling and updating the TopNav Bar, When the rest of the page is finished, return and add the necessary router-links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
     <div id="nav">
-      <nav class="black" style="padding-bottom:12%;" role="navigation">
+      <nav class="topNav" style="padding-bottom:12%;" role="navigation">
               <div class="nav-wrapper">
-                <a href="#" class="right brand-logo" style="padding-right:1%;">TheOtherSide</a>
+                <a href="#" class="right brand-logo hide-on-med-and-down"  style="padding-right:1%;"><img id="title" :src="Images.title"></a>
+                 <a href="#" class="center brand-logo hide-on-large-only"  style=""><img id="titleMobile" :src="Images.title"></a>
                 <!---This is the top section of the topNav---->
                 <ul id="nav-mobile" class="left hide-on-med-and-down">
                     <li><h5><i class="material-icons">home</i><b>Home</b></h5></li>
@@ -13,7 +14,7 @@
                 </div>
 <br><br>
                 <!---Bottom section of the Top Nav--->
-                  <div class="row black">
+                  <div class="row topNav"  style="">
                     <div class="nav-content hide-on-med-and-down">
                       <ul>
                         <li class="tab"><h6>Timeline</h6></li>
@@ -28,30 +29,75 @@
 </template>
 
 <style scoped>
+    /*Area for the top nav*/
     nav{
         position: fixed;
         top:0%;
         width: 100%;
         z-index: 5;
+        
     }
 
+    /*Change the background to the topNav*/
+    .topNav{
+        background-image: linear-gradient(to right,#4B1411,black);
+    
+    }
+    /*Handle the area for the mobile Nav*/
     #nav-mobile{
         width: 75%;
     }
+
+ 
+    /*Each of the list items in the nav bar*/
     li{
         padding-right:5%;
         padding-left:2%;
     }
 
+    /*When user hovers over each of the H5 elements*/
     h5:hover{
         cursor: pointer;
-        color: #EA8D55;
-    }
-
-    h5{
+        color:coral;
         
     }
+    /*Change the font to the main menu items (Main ones)*/
+    h5{
+       color:white; 
+       font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+    }
 
-    /*Handle hover over each Nav button*/
+    /*Change the font to the main menu items (Submenu)*/
+    h6{
+        font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        font-size: 140%;
+    }
+
+    /*Add styling to the title of the page*/
+    #title{ /*This handles the banner for the desktop*/
+        width: 20%;
+        position: fixed;
+        right:2%;
+        top: 2%;
+    }
+
+  
+    /*For the mobile banner, tablets and phones*/
+    #titleMobile{
+        width: 50%;
+    }
+   
 
 </style>
+
+<script>
+export default {
+    data(){
+        return{
+            Images:{
+                title: require("/Users/atakoramuriel/Desktop/tosh/Img/Title.png"), //This is for the image of TheOtherSide
+            }
+        }
+    }
+}
+</script>
