e6e5c1d55e9a15775a92fc2fa88f13340fec9cbc
Lord Atakora <inquire@obsidianrune.com>
2020-07-01 19:40:27 -0400

    Fixed issue with the nav so the home page will load correctly

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
                  <a href="#" class="center brand-logo hide-on-large-only"  style=""><img id="titleMobile" :src="Images.title"></a>
                 <!---This is the top section of the topNav---->
                 <ul id="nav-mobile" class="left hide-on-med-and-down">
-                    <li><h5><i class="material-icons">home</i><b><router-link to="/" style="font-size:95%;">Home</router-link></b></h5></li>
+                    <li><h5 v-on:click="reload()"><i class="material-icons">home</i><b><router-link to="/" style="font-size:95%;">Home</router-link></b></h5></li>
                     <li><h5><i class="material-icons">import_contacts</i><b><router-link to="/story" style="font-size:95%;">Story</router-link></b></h5></li>
                     <li><h5><i class="material-icons">people_outline</i><b><router-link to="/social" style="font-size:95%;">Social</router-link></b></h5></li>
                     <li><h5><i class="material-icons">redeem</i><b><router-link to="/shop" style="font-size:95%;">Shop</router-link></b></h5></li>
@@ -103,6 +103,12 @@ export default {
             Images:{
                 title: require("/Users/atakoramuriel/Desktop/tosh/Img/Title.png"), //This is for the image of TheOtherSide
             }
+        }
+    },
+    methods: {
+        reload(){
+            //this.$router.push("/");
+            location.reload();
         }
     }
 }
